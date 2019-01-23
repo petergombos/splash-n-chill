97bb064d851cc95259fe5309b0e7fb84d32687c6
Peter Gombos <peter@tabeo.co.uk>
2019-01-23 11:46:24 +0100

    Implement basic pagination and image prefetching.

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -7,15 +7,59 @@ import api from "../utils/api.js";
 export default class Browser extends Component {
   state = {
     photos: null,
-    currentIndex: 0
+    currentIndex: 0,
+    currentPage: 0
   };
 
-  async componentDidMount() {
-    const {data} = await api.get("/photos");
-    this.setState({
-      photos: data
-    });
+  componentDidMount() {
+    document.onkeydown = this.handleKeyDown;
+    this.fetchNextBatch();
   }
+
+  componentWillUnmount() {
+    document.onkeydown = undefined;
+  }
+
+  handleKeyDown = e => {
+    const {photos, currentIndex} = this.state;
+    if (e.keyCode === 37) {
+      this.setState(state => ({
+        currentIndex: Math.max(state.currentIndex - 1, 0)
+      }));
+    } else if (e.keyCode === 39) {
+      if (photos.length - 2 === currentIndex) {
+        this.fetchNextBatch();
+      }
+      this.setState(state => ({
+        currentIndex: Math.min(state.currentIndex + 1, state.photos.length - 1)
+      }));
+    }
+  };
+
+  isFetching = false;
+  fetchNextBatch = async () => {
+    if (this.isFetching) {
+      return;
+    } else {
+      this.isFetching = true;
+    }
+    const {currentPage} = this.state;
+    const nextPage = currentPage + 1;
+    const {data} = await api.get(`/photos?page=${nextPage}`);
+    this.preFetchImages(data);
+    this.setState(state => ({
+      photos: state.photos ? [...state.photos, ...data] : data,
+      currentPage: nextPage
+    }));
+    this.isFetching = false;
+  };
+
+  preFetchImages = images => {
+    images.forEach(image => {
+      const img = new Image();
+      img.src = image.urls.regular;
+    });
+  };
 
   render() {
     const {photos, currentIndex} = this.state;
@@ -24,6 +68,6 @@ export default class Browser extends Component {
       return <Text>Loading...</Text>;
     }
 
-    return <Photo data={photos[currentIndex]} />;
+    return <Photo key={photos[currentIndex].id} data={photos[currentIndex]} />;
   }
 }
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -11,13 +11,31 @@ export class Photo extends Component {
     data: PropTypes.object.isRequired
   };
 
+  state = {
+    isFullLoaded: false
+  };
+
+  componentDidMount() {
+    const {
+      data: {urls}
+    } = this.props;
+    this.full = new Image();
+    this.full.onload = () => {
+      this.setState({
+        isFullLoaded: true
+      });
+    };
+    this.full.src = urls.full;
+  }
+
   render() {
     const {
       data: {urls, user}
     } = this.props;
+    const {isFullLoaded} = this.state;
     return (
       <View
-        backgroundImage={`url(${urls.full})`}
+        backgroundImage={`url(${isFullLoaded ? this.full.src : urls.regular})`}
         backgroundPosition="center"
         backgroundSize="cover"
         height="100%"
