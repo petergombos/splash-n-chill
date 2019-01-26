0caa3f6b7bb7b66e91761d79ac2c823348172641
Peter Gombos <peter@tabeo.co.uk>
2019-01-26 14:38:27 +0100

    Add infinite scrolling to Collections.

--- src/pages/Home/Collections.js
+++ src/pages/Home/Collections.js
@@ -6,17 +6,48 @@ import {Title, Heading, Caption} from "../../components/Text";
 import GalleryLink from "../../components/GalleryLink";
 import api from "../../utils/api";
 
+function getDocHeight() {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight
+  );
+}
+
 export class Collections extends Component {
   state = {
-    collections: null
+    collections: null,
+    currentPage: 1
   };
 
-  async componentDidMount() {
-    const collections = await api.get("/collections");
-    this.setState({
-      collections
-    });
+  componentDidMount() {
+    this.handleFetch();
+    document.addEventListener("scroll", this.handleScroll);
   }
+
+  handleScroll = e => {
+    const offset = window.pageYOffset;
+    const docHeight = getDocHeight();
+    const distanceToBottom = docHeight - offset - window.innerHeight;
+    if (distanceToBottom < window.innerHeight && !this.isFetching) {
+      this.handleFetch();
+    }
+  };
+
+  isFetching = false;
+  handleFetch = async () => {
+    this.isFetching = true;
+    const {currentPage, collections} = this.state;
+    const data = await api.get(`/collections?page=${currentPage}`);
+    this.setState({
+      collections: collections ? [...collections, ...data] : data,
+      currentPage: currentPage + 1
+    });
+    this.isFetching = false;
+  };
 
   render() {
     const {collections} = this.state;
